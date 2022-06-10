6360e7446d60fc0262b20d631b8b6d49ecc237be
alejandrobelt <alejandrobelt@gmail.com>
2022-06-10 12:04:38 -0300

    package json

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularMaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+import { EditItemComponent } from './components/edit-item/edit-item.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/* Firebase */
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
+import { ItemService } from './shared/item.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddItemComponent,
+    EditItemComponent,
+    ItemListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule
+  ],
+  providers: [ItemService],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class AppModule { }
